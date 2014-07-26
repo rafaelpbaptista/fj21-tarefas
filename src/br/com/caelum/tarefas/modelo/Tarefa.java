6d860504c73e9169654f90e30c888f7ce1f7f5d3
rafaelpbaptista <rafael.baptista@caelum.com.br>
2014-07-25 23:25:12 -0300

    Capitulo 14

--- src/br/com/caelum/tarefas/modelo/Tarefa.java
+++ src/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -2,22 +2,28 @@ package br.com.caelum.tarefas.modelo;
 
 import java.util.Calendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-//@Entity
+@Entity
 public class Tarefa {
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@NotNull
 	@Size(min = 5)
 	private String descricao;
 	private boolean finalizado;
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
-//	@Temporal(TemporalType.DATE)
+	@Temporal(TemporalType.DATE)
 	private Calendar dataFinalizacao;
 
 	public Long getId() {
